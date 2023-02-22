092a3623b00d78c2be5ead26ac34fe1d9807bd86
hatobanono <chris00iq@gmail.com>
2023-02-22 14:35:38 +0800

    API串接、檔案建立

---
+++ The-fifth-group-FinalAPI/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace The_fifth_group_FinalAPI
+{
+	public class WeatherForecast
+	{
+		public DateTime Date { get; set; }
+
+		public int TemperatureC { get; set; }
+
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+		public string? Summary { get; set; }
+	}
+}
