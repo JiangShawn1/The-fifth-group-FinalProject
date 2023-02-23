edbb2b245a2c96f127102dfcf10e7042ddfe8391
“Tony” <tunglin.yang1@gmail.com>
2023-02-23 16:03:28 +0800

    202302231603

---
+++ The-fifth-group-FinalProject/Controllers/WebSocketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace The_fifth_group_FinalProject.Controllers
+{
+    public class WebSocketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
