1d055f4d24f4aa70262e9afee272ad0ebe345124
“Tony” <tunglin.yang1@gmail.com>
2023-03-02 16:39:39 +0800

    WebSocket 20230302 1639

--- The-fifth-group-FinalProject/Controllers/WebSocketController.cs
+++ The-fifth-group-FinalProject/Controllers/WebSocketController.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections.Concurrent;
 using System.Net.WebSockets;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks.Dataflow;
 
 namespace The_fifth_group_FinalProject.Controllers
 {
     public class WebSocketController : Controller
     {
-        static ConcurrentDictionary<int, WebSocket> Websockets = new ConcurrentDictionary<int, WebSocket>();
+        static ConcurrentDictionary<int, WebSocket> WebSockets = new ConcurrentDictionary<int, WebSocket>();
+
         public IActionResult Index()
         {
             return View();
         }
-        [Route("/ws")]
+
+        [Route("{Controller}/ws")]
         public async Task Get()
         {
             if (HttpContext.WebSockets.IsWebSocketRequest)
@@ -27,36 +32,49 @@ namespace The_fifth_group_FinalProject.Controllers
             }
         }
 
-        private async Task ProcessWebSocket(WebSocket webSocket)
+        public async Task ProcessWebSocket(WebSocket webSocket)
         {
-             Websockets.TryAdd(webSocket.GetHashCode(), webSocket);
+            WebSockets.TryAdd(webSocket.GetHashCode(), webSocket);
             var buffer = new byte[1024 * 4];
-            var res = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer),CancellationToken.None);
-            var userName = "anoymous";
+            var res = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            string? UserName = null;
             while (!res.CloseStatus.HasValue)
             {
+                UserName = "匿名";
                 var cmd = Encoding.UTF8.GetString(buffer, 0, res.Count);
-                if(!string.IsNullOrEmpty(cmd))
+                JObject data = JObject.Parse(cmd);
+                string? Name = Convert.ToString(data["userName"]);
+                string? Message = $"{Convert.ToString(data["message"])} at {DateTime.Now}";
+                if (!string.IsNullOrEmpty(Name))
                 {
-                    if (cmd.StartsWith("/User")) userName = cmd.Substring(6);
-                    else Broadcast($"{userName} say : {cmd}");
+                    UserName = Name;
                 }
+                Broadcast(JsonConvert.SerializeObject(new
+                {
+                    userName = UserName,
+                    message = Message
+                }));
                 res = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                await webSocket.CloseAsync(res.CloseStatus.Value, res.CloseStatusDescription,CancellationToken.None);
-                Websockets.TryRemove(webSocket.GetHashCode(), out var removed);
-                Broadcast($"{userName} lest the room");
-            } 
+            }
+            await webSocket.CloseAsync(res.CloseStatus.Value, res.CloseStatusDescription, CancellationToken.None);
+            WebSockets.TryRemove(webSocket.GetHashCode(), out var removed); // removed允許未宣告
+            Broadcast(JsonConvert.SerializeObject(new
+            {
+                userName = UserName,
+                message = "離開聊天室"
+            }));
         }
 
-        private void Broadcast(string message)
+        public void Broadcast(string message)
         {
-            var buff = Encoding.UTF8.GetBytes($"{message} at {DateTime.Now}");
+            var buff = Encoding.UTF8.GetBytes($"{message}");
             var data = new ArraySegment<byte>(buff, 0, buff.Length);
-            Parallel.ForEach(Websockets.Values, async (WebSocket) =>
+            Parallel.ForEach(WebSockets.Values, async (webSocket) =>
             {
-                if(WebSocket.State == WebSocketState.Open) 
-                    await WebSocket.SendAsync(data, WebSocketMessageType.Text, true, CancellationToken.None);
+                if (webSocket.State == WebSocketState.Open)
+                    await webSocket.SendAsync(data, WebSocketMessageType.Text, true, CancellationToken.None);
             });
         }
     }
 }
+
