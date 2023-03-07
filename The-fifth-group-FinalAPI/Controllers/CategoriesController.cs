f089fa2a66302f6eded94275dc3246d5c63dedfd
JiangShawn1 <brm.259@gmail.com>
2023-03-07 14:26:53 +0800

    新增賽事Categories下拉選單API Vue完成

---
+++ The-fifth-group-FinalAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using The_fifth_group_FinalAPI.DTOs;
+using The_fifth_group_FinalAPI.Models;
+
+namespace The_fifth_group_FinalAPI.Controllers
+{
+	[EnableCors("AllowAny")]
+	[Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly TheFifthGroupOfTopicsContext _context;
+
+        public CategoriesController(TheFifthGroupOfTopicsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<IEnumerable<CategoriesDTO>> GetCategories()
+        {
+            return  _context.Categories.Select(c=>new CategoriesDTO
+            {
+                Id = c.Id,
+                Category= c.Category,
+                Distance= c.Distance,
+            }).OrderBy(c=>c.Distance);
+        }		
+
+		// GET: api/Categories/5
+		[HttpGet("{id}")]
+        public async Task<ActionResult<Categories>> GetCategories(int id)
+        {
+            var categories = await _context.Categories.FindAsync(id);
+
+            if (categories == null)
+            {
+                return NotFound();
+            }
+
+            return categories;
+        }
+
+        // PUT: api/Categories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategories(int id, Categories categories)
+        {
+            if (id != categories.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categories).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Categories>> PostCategories(Categories categories)
+        {
+            _context.Categories.Add(categories);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategories", new { id = categories.Id }, categories);
+        }
+
+        // DELETE: api/Categories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategories(int id)
+        {
+            var categories = await _context.Categories.FindAsync(id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(categories);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoriesExists(int id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ The-fifth-group-FinalAPI/DTOs/CategoriesDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace The_fifth_group_FinalAPI.DTOs
+{
+	public class CategoriesDTO
+	{
+		public int Id { get; set; }
+		public string? Category { get; set; }
+		public int Distance { get; set; }
+	}
+}
