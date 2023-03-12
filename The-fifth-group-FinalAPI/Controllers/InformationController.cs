789642679d1f409707bbf49f8d96da6b263c89be
JiangShawn1 <brm.259@gmail.com>
2023-03-12 17:20:48 +0800

    新增InformationAPI  報名表顯示參賽人資料

---
+++ The-fifth-group-FinalAPI/Controllers/InformationController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using The_fifth_group_FinalAPI.DTOs;
+using The_fifth_group_FinalAPI.Models;
+
+namespace The_fifth_group_FinalAPI.Controllers
+{
+	[EnableCors("AllowAny")]
+	[Route("api/[controller]")]
+    [ApiController]
+    public class InformationController : ControllerBase
+    {
+        private readonly TheFifthGroupOfTopicsContext _context;
+
+        public InformationController(TheFifthGroupOfTopicsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Information
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Information>>> GetInformation()
+        {
+            return await _context.Information.ToListAsync();
+        }
+
+        // GET: api/Information/5
+        [HttpGet("{id}")]
+        public async Task<InformationDTO> GetInformation(int id)
+        {
+            var information = await _context.Information.FindAsync(id);
+
+            if (information == null)
+            {
+                return null;
+            }
+			InformationDTO InfDTO = new InformationDTO
+			{
+				Id = information.Id,
+				Name = information.Name,
+				Phone = information.Phone,
+				Gender = information.Gender,
+                Address= information.Address,
+			};
+			return InfDTO;
+			
+        }
+		
+
+		// PUT: api/Information/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+        public async Task<IActionResult> PutInformation(int id, Information information)
+        {
+            if (id != information.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(information).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InformationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Information
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Information>> PostInformation(Information information)
+        {
+            _context.Information.Add(information);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetInformation", new { id = information.Id }, information);
+        }
+
+        // DELETE: api/Information/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInformation(int id)
+        {
+            var information = await _context.Information.FindAsync(id);
+            if (information == null)
+            {
+                return NotFound();
+            }
+
+            _context.Information.Remove(information);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool InformationExists(int id)
+        {
+            return _context.Information.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ The-fifth-group-FinalAPI/DTOs/InformationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace The_fifth_group_FinalAPI.DTOs
+{
+	public class InformationDTO
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Phone { get; set; }
+		public bool Gender { get; set; }
+		public string? Address { get; set; }
+	}
+}
