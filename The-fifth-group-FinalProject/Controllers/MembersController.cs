2f8793e6dc62c7687b35067fc7a192b2971355e0
Erknard <Erknard2015@gmail.com>
2023-03-16 18:31:26 +0800

    增加功能

---
+++ The-fifth-group-FinalProject/Controllers/MembersController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using The_fifth_group_FinalProject.Models;
+
+namespace The_fifth_group_FinalProject.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly TheFifthGroupOfTopicsContext _context;
+
+        public MembersController(TheFifthGroupOfTopicsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Members
+        public async Task<IActionResult> Index()
+        {
+            var httpContext = HttpContext;
+            int MemId = int.TryParse(httpContext.Request.Cookies["MemberId"], out int result) ? result : 2;
+            var members = await _context.Members.Where(m => m.MemberId == MemId).ToListAsync();
+            return RedirectToAction("Details", new { id = MemId });
+        }
+
+        // GET: Members/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Members == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Members
+                .FirstOrDefaultAsync(m => m.MemberId == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return View(member);
+        }
+
+        // GET: Members/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Members/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MemberId,Name,Account,Password,Phone,Mail,Subscription,IsConfirmed,Freeze,State")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(member);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+
+        // GET: Members/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Members == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Members.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return View(member);
+        }
+
+        // POST: Members/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MemberId,Name,Account,Password,Phone,Mail,Subscription,IsConfirmed,Freeze,State")] Member member)
+        {
+            if (id != member.MemberId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(member);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MemberExists(member.MemberId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+
+        // GET: Members/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Members == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Members
+                .FirstOrDefaultAsync(m => m.MemberId == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return View(member);
+        }
+
+        // POST: Members/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Members == null)
+            {
+                return Problem("Entity set 'TheFifthGroupOfTopicsContext.Members'  is null.");
+            }
+            var member = await _context.Members.FindAsync(id);
+            if (member != null)
+            {
+                _context.Members.Remove(member);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MemberExists(int id)
+        {
+          return (_context.Members?.Any(e => e.MemberId == id)).GetValueOrDefault();
+        }
+    }
+}
