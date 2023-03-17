4b62258d51223f66c379ea358edad6899ad6d38c
hatobanono <chris00iq@gmail.com>
2023-03-17 13:39:27 +0800

    1339

--- The-fifth-group-FinalProject/Controllers/OrdersController.cs
+++ The-fifth-group-FinalProject/Controllers/OrdersController.cs
@@ -161,6 +161,7 @@ namespace The_fifth_group_FinalProject.Controllers
                     }
 
                     data.OrderItem = JsonConvert.DeserializeObject<List<OrderItem>>(data.OrderContent);
+                    ViewBag.orderItems = data.OrderItem;
 
                     return View(data);
                 }
